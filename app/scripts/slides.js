b3ac77c68d6122787423178a3f51e64829d25d9f
Srimal Ruwange <srimal@calcey.com>
2014-04-21 15:54:03 +0530

    hide menu when touch move

--- app/scripts/slides.js
+++ app/scripts/slides.js
@@ -152,7 +152,8 @@ $(document).on('onTemplateRenderComplete', function () {
             $('footer div.chapters div.chapter a').eq(chapterNo-1).addClass('selected');
             $slideThumbs.trigger('owl.goTo', (slideNo - 1));
             slideThumb.eq(slideNo - 1).addClass('slide-selected');
-            $slideThumbs.show();
+            $slideThumbs.fadeIn();
+			$('.owl-wrapper-outer',$chapters).fadeIn();
         });
         return false;
     });
@@ -198,4 +199,15 @@ $(document).on('onTemplateRenderComplete', function () {
         }, 500);
         return false;
     });
+	
+	document.addEventListener('touchmove', function(e) 
+	{
+		var container = $('.chapter-menu');
+		if (!container.is(e.target) && container.has(e.target).length === 0)
+		{
+			$slideThumbs.hide();
+			$('.owl-wrapper-outer',$chapters).hide();
+        	$('.chapter-menu').slideUp();
+		}
+	});
 });
