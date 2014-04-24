8474cb86ee2269a5ab8723dbc1fdb5365f19e22c
Premuditha Indunil <premuditha@calcey.com>
2014-04-24 16:20:16 +0530

    Mofidied gulp build script.

--- gulpfile.js
+++ gulpfile.js
@@ -63,16 +63,8 @@ gulp.task('images', function () {
 });
 
 gulp.task('fonts', function () {
-    return $.bowerFiles()
-        .pipe($.filter([
-            '**/*.eot',
-            '**/*.svg',
-            '**/*.ttf',
-            '**/*.woff'
-        ]))
-        .pipe($.flatten())
-        .pipe(gulp.dest('dist/fonts'))
-        .pipe($.size());
+    return gulp.src('app/styles/fonts/**/*')
+        .pipe(gulp.dest('dist/styles/fonts'));
 });
 
 // Clean
