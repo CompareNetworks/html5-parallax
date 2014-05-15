2406b71124a4de6109e3386fb5dab0a4802549af
brianlocicero <blocicero@Brians-MacBook-Air.local>
2014-05-15 15:07:44 -0700

    Parallax?

--- app/data/presentation-content/universal/universalScripts.js
+++ app/data/presentation-content/universal/universalScripts.js
@@ -6,14 +6,23 @@ $(function () {
         var swiper = event.horizontalSwiperObj;
         //Plugin adds "progress" property to each slide and common "progress" property for swiper
         for (var i = 0; i < swiper.slides.length; i++) {
+
           var slide = swiper.slides[i];
-          //console.log(slide);
+
           var slideProgress = slide.progress;
           //console.log(slideProgress);
           //Do something depending on slideProgress
         }
         var swiperProgress = swiper.progress;
-        console.log( swiperProgress * 100 );
+        //console.log(swiperProgress);
+        var modSwiperProgress = -( Math.floor(swiperProgress * 2000 ) );
+
+
+        //move('.brianWave').x(modSwiperProgress).duration(300).end();
+        $(".brianWave").css("left", "" + modSwiperProgress + "px");
+        $(".hairBubbles1").css("left", "" + (modSwiperProgress * -.8) + "px");
+
+
         //Do something with common swiper progress
       }
     }
